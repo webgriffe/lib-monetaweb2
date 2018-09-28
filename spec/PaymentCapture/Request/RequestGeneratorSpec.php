f618b5eadcff7c16819af844fe7690a27297b7f2
Andrea Zambon <azambon@webgriffe.com>
2018-09-28 18:00:17 +0200

    WIP Adds some tests for the new capture features

---
+++ spec/PaymentCapture/Request/RequestGeneratorSpec.php
@@ -0,0 +1,159 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kraken
+ * Date: 28/09/18
+ * Time: 17.37
+ */
+
+namespace spec\Webgriffe\LibMonetaWebDue\PaymentCapture\Request;
+
+use PhpSpec\ObjectBehavior;
+use Webgriffe\LibMonetaWebDue\LogicRequestDataContainerInterface;
+use Webgriffe\LibMonetaWebDue\PaymentCapture\Request\RequestGenerator;
+
+class RequestGeneratorSpec extends ObjectBehavior
+{
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(RequestGenerator::class);
+    }
+
+    public function it_should_generate_request_data_with_valid_params()
+    {
+        $this->generate(
+            'https://url.com/',
+            '99999999',
+            '99999999',
+            59.00,
+            'EUR',
+            '000000156',
+            '841826491344182719'
+        )
+            ->shouldHaveType(LogicRequestDataContainerInterface::class);
+    }
+
+    public function it_should_generate_request_data_with_the_same_url_that_was_provided()
+    {
+        $this->generate(
+            'https://url.com/',
+            '99999999',
+            '99999999',
+            59.00,
+            'EUR',
+            '000000156',
+            '841826491344182719'
+        )
+            ->getUrl()->shouldBeEqualTo('https://url.com/');
+    }
+
+    public function it_should_generate_request_data_with_post_method()
+    {
+        $this->generate(
+            'https://url.com/',
+            '99999999',
+            '99999999',
+            59.00,
+            'EUR',
+            '000000156',
+            '841826491344182719'
+        )
+            ->getMethod()->shouldBeEqualTo('POST');
+    }
+
+    public function it_should_generate_request_data_with_correct_parameters()
+    {
+        $this->generate(
+            'https://url.com/',
+            '99999999',
+            '99999999',
+            59.00,
+            'EUR',
+            '000000156',
+            '841826491344182719'
+        )
+            ->getParams()->shouldBeEqualTo(
+                [
+                    'id' => '99999999',
+                    'password' => '99999999',
+                    'operationType' => 'confirm',
+                    'amount' => '59.00',
+                    'currencyCode' => '978',
+                    'merchantOrderId' => '000000156C',
+                    'paymentId' => '841826491344182719',
+                    'customField' => null,
+                    'description' => null,
+                ]
+            );
+    }
+
+    public function it_should_throw_exception_if_url_is_missing()
+    {
+        $this->shouldThrow(\InvalidArgumentException::class)
+            ->duringGenerate(
+                '',
+                '99999999',
+                '99999999',
+                59.00,
+                'EUR',
+                '000000156',
+                '841826491344182719'
+            );
+    }
+
+    public function it_should_throw_exception_if_terminal_id_is_missing()
+    {
+        $this->shouldThrow(\InvalidArgumentException::class)
+            ->duringGenerate(
+                'https://url.com/',
+                '',
+                '99999999',
+                59.00,
+                'EUR',
+                '000000156',
+                '841826491344182719'
+            );
+    }
+
+    public function it_should_throw_exception_if_terminal_password_is_missing()
+    {
+        $this->shouldThrow(\InvalidArgumentException::class)
+            ->duringGenerate(
+                'https://url.com/',
+                '99999999',
+                '',
+                59.00,
+                'EUR',
+                '000000156',
+                '841826491344182719'
+            );
+    }
+
+    public function it_should_throw_exception_if_amount_is_null()
+    {
+        $this->shouldThrow(\InvalidArgumentException::class)
+            ->duringGenerate(
+                'https://url.com/',
+                '99999999',
+                '99999999',
+                null,
+                'EUR',
+                '000000156',
+                '841826491344182719'
+            );
+    }
+
+    public function it_should_throw_exception_if_amount_is_zero()
+    {
+        $this->shouldThrow(\InvalidArgumentException::class)
+            ->duringGenerate(
+                'https://url.com/',
+                '99999999',
+                '99999999',
+                0,
+                'EUR',
+                '000000156',
+                '841826491344182719'
+            );
+    }
+}
