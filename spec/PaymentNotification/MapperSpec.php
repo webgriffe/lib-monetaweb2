5b3c01e1cf048222772237583edb9805521f00c2
Andrea Zambon <azambon@webgriffe.com>
2018-10-11 15:37:32 +0200

    Adds some specs for the notify mapper

--- spec/PaymentNotification/MapperSpec.php
+++ spec/PaymentNotification/MapperSpec.php
@@ -5,6 +5,7 @@ namespace spec\Webgriffe\LibMonetaWebDue\PaymentNotification;
 use GuzzleHttp\Psr7\ServerRequest;
 use PhpSpec\ObjectBehavior;
 use Webgriffe\LibMonetaWebDue\PaymentNotification\Mapper;
+use Webgriffe\LibMonetaWebDue\PaymentNotification\Result\PaymentResultErrorInfo;
 use Webgriffe\LibMonetaWebDue\PaymentNotification\Result\PaymentResultInfo;
 use Webgriffe\LibMonetaWebDue\PaymentNotification\Result\CCPaymentResultInterface;
 use Webgriffe\LibMonetaWebDue\PaymentNotification\Result\MyBankPaymentResultInfo;
@@ -54,6 +55,34 @@ class MapperSpec extends ObjectBehavior
         $this->map($request)->shouldBeLike($expectedPaymentResult);
     }
 
+    public function it_should_create_mybank_payment_result_info_with_approved_mybank_transaction()
+    {
+        $request = new ServerRequest('POST', 'any uri');
+        $parsedBody = [
+            'paymentid' => '123456789012345678',
+            'result' => 'AUTHORISED',
+            'description' => 'desc',
+            'authorizationcode' => '85963',
+            'merchantorderid' => 'TRCK0001',
+            'mybankid' => '9876543210',
+            'customfield' => 'some custom field',
+            'securitytoken' => '80957febda6a467c82d34da0e0673a6e',
+        ];
+        $request = $request->withParsedBody($parsedBody);
+
+        $expectedPaymentResult = new MyBankPaymentResultInfo(
+            '123456789012345678',
+            'AUTHORISED',
+            'desc',
+            '85963',
+            'TRCK0001',
+            '9876543210',
+            'some custom field',
+            '80957febda6a467c82d34da0e0673a6e'
+        );
+        $this->map($request, 'initializemybank')->shouldBeLike($expectedPaymentResult);
+    }
+
     public function it_should_create_payment_result_info_with_canceled_transaction()
     {
         $request = new ServerRequest('POST', 'any uri');
@@ -82,44 +111,44 @@ class MapperSpec extends ObjectBehavior
         $this->map($request)->shouldBeLike($expectedPaymentResult);
     }
 
+    public function it_should_create_error_payment_result_info_with_error_response()
+    {
+        $request = new ServerRequest('POST', 'any uri');
+        $parsedBody = [
+            'paymentid' => '123456789012345678',
+            'errorcode' => 'code',
+            'errormessage' => 'message'
+        ];
+        $request = $request->withParsedBody($parsedBody);
+
+        $expectedPaymentResult = new PaymentResultErrorInfo('code', 'message', '123456789012345678');
+        $this->map($request)->shouldBeLike($expectedPaymentResult);
+    }
+
     public function it_should_throw_exception_when_required_fields_are_missing()
     {
         $request = new ServerRequest('POST', 'any uri');
-        $this->shouldThrow(\InvalidArgumentException::class)->duringMap($request->withParsedBody([]));
-        $this->shouldThrow(\InvalidArgumentException::class)->duringMap($request->withParsedBody(['paymentid' => '1']));
+        $this->shouldThrow(\InvalidArgumentException::class)->duringMap(
+            $request->withParsedBody([])
+        );
+        $this->shouldThrow(\InvalidArgumentException::class)->duringMap(
+            $request->withParsedBody(['paymentid' => '1'])
+        );
         $this->shouldThrow(\InvalidArgumentException::class)->duringMap(
             $request->withParsedBody(['paymentid' => '1', 'result' => '000'])
         );
         $this->shouldThrow(\InvalidArgumentException::class)->duringMap(
             $request->withParsedBody(['threedsecure' => '1', 'result' => '000'])
         );
-    }
 
-    public function it_should_create_mybank_payment_result_info_with_approved_mybank_transaction()
-    {
-        $request = new ServerRequest('POST', 'any uri');
-        $parsedBody = [
-            'paymentid' => '123456789012345678',
-            'result' => 'AUTHORISED',
-            'description' => 'desc',
-            'authorizationcode' => '85963',
-            'merchantorderid' => 'TRCK0001',
-            'mybankid' => '9876543210',
-            'customfield' => 'some custom field',
-            'securitytoken' => '80957febda6a467c82d34da0e0673a6e',
-        ];
-        $request = $request->withParsedBody($parsedBody);
-
-        $expectedPaymentResult = new MyBankPaymentResultInfo(
-            '123456789012345678',
-            'AUTHORISED',
-            'desc',
-            '85963',
-            'TRCK0001',
-            '9876543210',
-            'some custom field',
-            '80957febda6a467c82d34da0e0673a6e'
+        $this->shouldThrow(\InvalidArgumentException::class)->duringMap(
+            $request->withParsedBody(['mybankid' => '1'])
         );
-        $this->map($request, 'initializemybank')->shouldBeLike($expectedPaymentResult);
+        $this->shouldThrow(\InvalidArgumentException::class)->duringMap(
+            $request->withParsedBody(['mybankid' => '1', 'paymentid' => '1'])
+        );
+        $this->shouldThrow(\InvalidArgumentException::class)->duringMap(
+            $request->withParsedBody(['mybankid' => '1', 'result' => '000'])
+        );
     }
 }
